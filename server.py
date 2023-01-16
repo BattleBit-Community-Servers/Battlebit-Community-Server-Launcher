ee4dafc6849e2fc9c834db882978e1b52eed2cc0
Julian Ellis <business.juliane+KeyBase@gmail.com>
2023-01-16 01:16:22 -0500

    Added Argument Option "-noupdate"
    This will skip the running of steamcmd before launching the server to skip updating

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import subprocess
 from dotenv import load_dotenv
 import os
+import sys
 
 load_dotenv()
 server_vars = ["SERVER_PATH", "SERVER_NAME", "SERVER_PORT", "SERVER_HZ", "SERVER_SAVE_STATS", "SERVER_DYNAMIC_ROOMS", "SERVER_MAX_PLAYERS", "SERVER_PLAYERS_TO_START", "SERVER_FIRST_ROOM", "SERVER_DYNAMIC_FIRST_MAP", "SERVER_DYNAMIC_FIRST_GAMEMODE", "SERVER_DYNAMIC_FIRST_SIZE", "SERVER_DYNAMIC_FIXED_SIZE", "SERVER_BEGINNER_SERVER", "SERVER_DYNAMIC_MAX_SIZE", "SERVER_RCON_ENABLED", "SERVER_RCON_PASSWORD", "SERVER_REGION", "SERVER_TESTER_SERVER"]
@@ -14,7 +15,8 @@ args.append("-nographics")
 
 folder_name = "SteamCMD"
 steamcmd_path = os.path.join(folder_name, "steamcmd.exe")
-subprocess.run([steamcmd_path, "+runscript", "update.txt"])
+if "-noupdate" not in sys.argv:
+    subprocess.run([steamcmd_path, "+runscript", "update.txt"])
 try:
     subprocess.run(args)
 except KeyboardInterrupt:
