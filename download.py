8db066ba83e8e95e2a01bdd7962ca39f7fa04447
Julian Ellis <business.juliane+KeyBase@gmail.com>
2023-05-30 05:26:18 -0400

    Added more comments

--- download.py
+++ download.py
@@ -1,13 +1,18 @@
-from termcolor import colored
 import os
 from dotenv import load_dotenv
 from main.scripts import create_file, download_and_extract
 
 print("created by jellisy")
 
+# Check for the presence of .env file
 if os.path.isfile('.env'):
+    # Load the environment variables
     load_dotenv()
+    
+    # Create the update.txt file
     create_file()
+    
+    # Download and extract the SteamCMD files, then execute the update.txt script
     download_and_extract()
 else:
-    print(colored('.env file not found, please create or edit the .env file first!', 'red'))
+    print('\033[91m.env file not found, please create or edit the .env file first!\033[0m')
