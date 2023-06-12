d55a6ff0a60f905cd69e918b8b36b4a4e0412dd5
Julian Ellis <business.juliane+KeyBase@gmail.com>
2023-06-12 13:43:35 -0400

    Create server.bat

---
+++ server.bat
@@ -0,0 +1,13 @@
+@echo off
+echo Python is installed.
+
+if exist requirements.txt (
+    echo Installing Python packages from requirements.txt...
+    python3 -m pip install -r requirements.txt
+) else (
+    echo requirements.txt not found. Skipping package installation.
+)
+
+echo Running server.py...
+python3 server.py
+pause
