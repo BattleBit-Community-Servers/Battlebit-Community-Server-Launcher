87bc50bdaca90fe192fcf6f172a1b5c3509d6f64
Julian Ellis <business.juliane+KeyBase@gmail.com>
2023-08-26 21:41:32 -0400

    Added Launch Arguments to run with or without API

---
+++ server+noapi.bat
@@ -0,0 +1,14 @@
+@echo off
+echo Python is installed.
+
+if exist requirements.txt (
+    echo Installing Python packages from requirements.txt...
+    python -m pip install -r requirements.txt
+) else (
+    echo requirements.txt not found. Skipping package installation.
+)
+
+echo Running server.py...
+python server.py
+pause
+g
--- server.py
+++ server.py
@@ -4,12 +4,37 @@ import os
 import sys
 
 load_dotenv()
-server_vars = [
-    "SERVER_NAME","SERVER_PORT","SERVER_HZ","SERVER_ANTICHEAT","SERVER_MAX_PING","SERVER_VOXEL_MODE","SERVER_API_ENDPOINT","SERVER_FIXED_SIZE","SERVER_FIRST_GAMEMODE","SERVER_FIRST_MAP"]
+
+# List of common server variables
+common_server_vars = [
+    "SERVER_NAME",
+    "SERVER_PORT",
+    "SERVER_HZ",
+    "SERVER_ANTICHEAT",
+    "SERVER_MAX_PING",
+    "SERVER_VOXEL_MODE",
+    "SERVER_FIXED_SIZE",
+    "SERVER_FIRST_GAMEMODE",
+    "SERVER_FIRST_MAP",
+]
+
+# List of server variables specific to -api
+api_server_vars = [
+    "SERVER_API_ENDPOINT",
+]
+
+# Check if the script is run with -api flag
+use_api = "-api" in sys.argv
+
+# Choose the appropriate set of server variables
+if use_api:
+    server_vars = common_server_vars + api_server_vars
+else:
+    server_vars = common_server_vars
 
 args = []
 for var in server_vars:
-    args.append(f"-{var.split('_', 1)[1].replace('_','')}={os.getenv(var)}")
+    args.append(f"-{var.split('_', 1)[1].replace('_', '')}={os.getenv(var)}")
 
 args.insert(0, os.getenv("SERVER_PATH"))
 args.append("-batchmode")
